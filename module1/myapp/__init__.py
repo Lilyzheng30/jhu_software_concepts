3118e5833924f26424869ce376f9febc36086a30
Lilyzheng30 <144749803+Lilyzheng30@users.noreply.github.com>
2026-01-21 01:41:46 -0500

    layout made

--- module1/myapp/__init__.py
+++ module1/myapp/__init__.py
@@ -1,20 +1,8 @@
 from flask import Flask 
 
-from myapp.pages import pages
+from jhu_software_concepts.module1.myapp import pages
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(pages.bp)
     return app
-
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route
-
-def index():
-    return render_template('base.html')
-
-if __name__ = __main__:
-    app.run(host='0.0.0.0', port=8080)
