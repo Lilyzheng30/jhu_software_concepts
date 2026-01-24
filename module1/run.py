96f5df238ad087e3d711193155ce1eb6801252c5
Lilyzheng30 <144749803+Lilyzheng30@users.noreply.github.com>
2026-01-24 14:11:15 -0500

    Delete module1 directory

--- module1/run.py
+++
@@ -1,7 +0,0 @@
-from myweb import create_app
-
-app = create_app()
-
-# Ensures the web server only starts when this file is run directly.
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080, debug=True)
