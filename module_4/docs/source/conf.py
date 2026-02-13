1acfe179fa12ba748a2b6a9b9e65291e565c4f54
Lilyzheng30 <144749803+Lilyzheng30@users.noreply.github.com>
2026-02-13 16:14:59 -0500

    html made

--- module_4/docs/source/conf.py
+++ module_4/docs/source/conf.py
@@ -17,7 +17,7 @@ release = '1.0'
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../src"))
+sys.path.insert(0, os.path.abspath("../../src"))
 
 extensions = [
     "sphinx.ext.autodoc",
